759cbb2d82eaa3cbab2ce6f1dff7a8eb897128b7
Josh <joshscina@gmail.com>
2024-08-20 17:30:22 -0400

    Rewrite: Upgrade to Tauri 2.0 requires a total rewrite of the application

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "~/components/ui/button";
+
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <Button>Test</Button>
+    </main>
+  );
+}
